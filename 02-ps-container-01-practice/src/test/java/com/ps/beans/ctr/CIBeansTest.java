8e73e68090232cc20c7eb0fcde57e42a35826eab
Mikhail Chtetsov <chtetsoff@gmail.com>
2018-01-02 18:41:42 +0300

    02-ps-container-01-practice

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -1,5 +1,7 @@
 package com.ps.beans.ctr;
 
+import com.ps.beans.ComplexBean;
+import java.util.Map;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +26,16 @@ public class CIBeansTest {
         for(String beanName :  ctx.getBeanDefinitionNames()) {
             logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
         }
+        ComplexBeanImpl complexBean0 = ctx.getBean("complexBean0", ComplexBeanImpl.class);
+        ComplexBeanImpl complexBean1 = ctx.getBean("complexBean1", ComplexBeanImpl.class);
+        ComplexBean2Impl complexBean2 = ctx.getBean("complexBean2", ComplexBean2Impl.class);
 
-        //TODO 3. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        assertNotNull(complexBean0.getSimpleBean());
+        assertNotNull(complexBean1.getSimpleBean());
+        assertTrue(complexBean1.isComplex());
+        assertNotNull(complexBean2.getSimpleBean1());
+        assertNotNull(complexBean2.getSimpleBean2());
+
+
     }
 }
